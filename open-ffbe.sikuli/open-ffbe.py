5c63f24b110a83f9bf8965a5598e6cf92e71aac3
Douglas Beal <public@dougbeal.com>
2017-04-18 13:25:12 -0700

    open ffbe

---
+++ open-ffbe.sikuli/open-ffbe.py
@@ -0,0 +1,16 @@
+from sikuli import *
+from org.sikuli.android import ADBScreen
+from org.sikuli.android import ADBDevice
+from pprint import pprint as pp
+tmpdir = os.path.join(getBundlePath(), "screencap")
+if not os.path.exists(tmpdir):
+  os.mkdir(tmpdir) 
+Debug.on(3)
+
+screen = ADBScreen.start() # get the one attached device
+if not screen:
+  exit(1)
+
+dev = screen.getDevice()
+use(screen) # set as the default region  
+
