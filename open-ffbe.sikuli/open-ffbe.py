b597e7cf25ad70027f903876cba05704ee620845
Douglas Beal <git@dougbeal.com>
2017-04-18 20:01:54 -0700

    go through targets in order

--- open-ffbe.sikuli/open-ffbe.py
+++ open-ffbe.sikuli/open-ffbe.py
@@ -4,18 +4,22 @@ from org.sikuli.android import ADBDevice
 from pprint import pprint as pp
 tmpdir = os.path.join(getBundlePath(), "screencap")
 if not os.path.exists(tmpdir):
-  os.mkdir(tmpdir) 
+    os.mkdir(tmpdir) 
 Debug.on(3)
 
 from .. import ffbe
 
 screen = ADBScreen.start() # get the one attached device
 if not screen:
-  exit(1)
+    exit(1)
 
 dev = screen.getDevice()
 use(screen) # set as the default region  
 
-start = ffbe.crops['ffbe-startscreen-tap']
-if exists(start.filename, start.getRegion()):
-  tap(start.getRegion().center())
+
+order = ['desktop-ffbe-icon-page-1-icon', 'ffbe-startscreen-tap']
+
+for image in order:
+    target = ffbe.crops[image]
+    if exists(target.filename, target.getRegion()):
+        tap(target.getRegion().center())
