1d147f2f16ada3e62345c7a13ae85acb1dcf4964
Douglas Beal <git@dougbeal.com>
2017-04-18 17:34:56 -0700

    split list comprenesion into loop

--- create-img-dict.sikuli/create-img-dict.py
+++ create-img-dict.sikuli/create-img-dict.py
@@ -3,34 +3,44 @@ from org.sikuli.android import ADBScreen
 from org.sikuli.android import ADBDevice
 from pprint import pprint as pp
 import json
-import fnmatch
-import glob
+from glob import glob
+from os.path import join, splitext, basename
 
-tmpdir = os.path.join(getBundlePath(), "screencap")
-base_img = os.path.join(getBundlePath(), "../img/base")
-json_filename = os.path.join(getBundlePath(), "image-region.json")
+tmpdir = join(getBundlePath(), "screencap")
+base_img = join(getBundlePath(), "../img/base")
+json_filename = join(getBundlePath(), "image-region.json")
 image_dict = {}
+json_file = None
 try:
     json_file = open(json_filename, 'w')
     image_dict = json.load(json_file)
 except (OSError, IOError) as e:
     print "json file doesn't exist yet."
 
+if json_file is None:
+    print "ERROR: failed to open json_file {}".format(json_filename)
+    exit(-1)
+
 Debug.on(3)    
-filenames = glob.glob(os.path.join(base_img, "*.png"))
+filenames = glob(join(base_img, "*.png"))
 print  "{} files found".format(len(filenames))
 for filename in filenames:
-    basename = os.path.splitext(os.path.basename(filename))[0]
+    basename = splitext(basename(filename))[0]
     finder = None
-    sections = list(name for name in filenames if filename is not name and os.path.splitext(os.path.basename(name))[0].startswith(basename))
-    print sections
-    for section in sections:
-        print "section " + section
+    cropped_images = []
+    for crop_filename in filenames:
+        crop_basename = splitext(basename(crop_filename))[0]
+        if filename is not crop_filename and crop_basename.startswith(basename):
+            cropped_images.append(crop_basename)
+        
+    print cropped_images
+    for cropped_image in cropped_images:
+        print "cropped_image " + cropped_image
         if finder is None:
             finder = Finder(filename)
-        print "matching {} in {}".format(section, filename)
-        # match section in filename
-        finder.findAll(section)
+        print "matching {} in {}".format(cropped_image, filename)
+        # match cropped_image in filename
+        finder.findAll(cropped_image)
         matches = sorted((m for m in finder), key=lambda m:m.getScore())
         
         #pp(matches)
@@ -39,5 +49,6 @@ for filename in filenames:
     if finder is not None:
         finder.destroy() # release the natives
     break
+
 
 json.dump(image_dict, json_file)
